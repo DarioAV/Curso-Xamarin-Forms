fd226d353f4a970dca384b7dd009b77cf7e1edf4
Darío Alonso Villar <dario_villar.97@hotmail.com>
2020-07-21 20:35:08 +0200

    Initial Commit

---
+++ App2.Android/CustomEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using App2.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry),typeof(CustomEntry))]
+namespace App2.Droid
+{
+    public class CustomEntry : EntryRenderer
+    {
+        public CustomEntry(Context context) : base(context)
+            {
+                
+            }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || e.NewElement == null)
+            {
+                return;
+            }
+            Control.Background = null;
+            if (!e.NewElement.IsPassword)
+            {
+                Control.InputType = InputTypes.TextFlagNoSuggestions;
+            }
+        }
+    }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            RectangleEmpresa.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+            RectangleEmpresa.Stroke = Color.FromHex("#c7cdd2");
+        }
+
+        private void Entry_Focused_1(object sender, FocusEventArgs e)
+        {
+            RectangleUsuario.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused_1(object sender, FocusEventArgs e)
+        {
+            RectangleUsuario.Stroke = Color.FromHex("#c7cdd2");
+        }
+
+        private void Entry_Focused_2(object sender, FocusEventArgs e)
+        {
+            RectangleContraseña.Stroke = Color.FromHex("#ce6767");
+        }
+
+        private void Entry_Unfocused_2(object sender, FocusEventArgs e)
+        {
+            RectangleContraseña.Stroke = Color.FromHex("#c7cdd2");
+        }
+    }
+}
